c40dc232e9239cf4f515ef8bef30fc7fb2add7ab
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2025-11-10 02:57:38 +0900

    feat: allow recursively #include-ing snippets

--- src/build/snippets.ts
+++ src/build/snippets.ts
@@ -3,28 +3,24 @@ import * as path from 'node:path';
 import type { Language } from '../language';
 
 export async function buildSnippets(language: Language): Promise<string> {
-    const snippets = fs.readFileSync(language.snippetPath, 'utf-8');
+    return recursivelyBuildSnippets(language.snippetPath);
+}
+
+function recursivelyBuildSnippets(snippetPath: string): string {
+    const snippets = fs.readFileSync(snippetPath, 'utf-8');
     return snippets
         .split('\n')
         .map(line => {
-            const included = getIncludeTarget(line);
-            if (!included) return line;
-            return includeSnippets(language.snippetPath, included);
+            const includedSnippetPath = getIncludedPath(snippetPath, line);
+            if (!includedSnippetPath) return line;
+            return recursivelyBuildSnippets(includedSnippetPath);
         })
         .join('\n');
 }
 
-function getIncludeTarget(line: string): string | null {
+function getIncludedPath(snippetPath: string, line: string): string | null {
     const match = line.match(/^#include "(.*)"\s*$/);
-    return match ? match[1] : null;
-}
-
-/**
- * @param source Path of the file including the target
- * @param target Path of the file included by the source
- */
-function includeSnippets(source: string, target: string) {
-    const includedPath = path.join(path.dirname(source), target);
-    const includedSnippets = fs.readFileSync(includedPath, 'utf-8');
-    return includedSnippets;
+    if (!match) return null;
+    const includedSnippetPath = path.join(path.dirname(snippetPath), match[1]);
+    return includedSnippetPath;
 }
